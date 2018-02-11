903d4bf443ab1aa7cae9081f060a48e210a48ff1
Jan Kristoffer Appel <jan.appel@gmail.com>
2018-02-11 22:40:56 +0100

    Added helper file describing device-specific database column mapping. For now, only Mi Band is included.

---
+++ device_db_mapping.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+device_db_mapping = {'MI_BAND': {'table': 'MI_BAND_ACTIVITY_SAMPLE', 
+                                'heartrate_col': 'HEART_RATE', 
+                                'timestamp_col': 'TIMESTAMP',
+                                'activity_col': 'RAW_KIND',
+                                'intensity_col': 'RAW_INTENSITY',
+                                'steps_col': 'STEPS'}}
